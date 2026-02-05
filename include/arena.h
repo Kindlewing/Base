46a5673e6627d69e1d26b22b556890c6399dd3fb
Hudson Finn <hudson.finn@zohomail.com>
2026-02-05 13:49:42 -0500

    Update arena.h

--- include/arena.h
+++ include/arena.h
@@ -11,10 +11,10 @@
 #define DEFAULT_ALIGNMENT (2 * sizeof(void *))
 #endif
 
-typedef struct {
-    u8 *base;
-    u64 capacity;
-    u64 offset;
+typedef struct arena {
+  u8 *base;
+  u64 capacity;
+  u64 offset;
 } arena;
 
 arena *arena_create(u64 capacity);
@@ -25,13 +25,13 @@ void *arena_push(arena *arena, u64 size);
 void *arena_push_aligned(arena *arena, u64 size, size_t align);
 void *arena_push_zero(arena *arena, u64 size);
 
-#define arena_push_struct(a, type) \
-    ((type *)arena_push_aligned((a), sizeof(type), DEFAULT_ALIGNMENT))
+#define arena_push_struct(a, type)                                             \
+  ((type *)arena_push_aligned((a), sizeof(type), DEFAULT_ALIGNMENT))
 
-#define arena_push_array(a, type, count) \
-    ((type *)arena_push_aligned((a), sizeof(type) * (count), DEFAULT_ALIGNMENT))
+#define arena_push_array(a, type, count)                                       \
+  ((type *)arena_push_aligned((a), sizeof(type) * (count), DEFAULT_ALIGNMENT))
 
-#define arena_push_struct_zero(a, type) \
-    ((type *)arena_push_zero(a, sizeof(type)))
+#define arena_push_struct_zero(a, type)                                        \
+  ((type *)arena_push_zero(a, sizeof(type)))
 
 #endif // ARENA_H
