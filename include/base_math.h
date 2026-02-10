70b12b3797424408000b4f715b601c0f2181ece9
Hudson Finn <hudson.finn@zohomail.com>
2026-02-09 20:55:30 -0500

    Math stuff

--- include/base_math.h
+++ include/base_math.h
@@ -11,10 +11,18 @@
 #define BASE_DEG2RAD 0.01745329251994329577f // PI/180
 #define BASE_RAD2DEG 57.2957795130823208768f // 180/PI
 
-typedef struct vector2f32_t {
+typedef struct vector2f32 {
 	f32 x;
 	f32 y;
 } vector2f32;
+
+typedef struct mat3x3f32 {
+	f32 m[9]; // idx: col * 3 + row
+} mat3x3f32;
+
+typedef struct mat4x4f32 {
+	f32 m[16]; // idx: col * 4 + row
+} mat4x4f32;
 
 static inline f32 sqrtf32(f32 x) {
 	__m128 t = _mm_set_ss(x);
@@ -22,15 +30,15 @@ static inline f32 sqrtf32(f32 x) {
 	return _mm_cvtss_f32(t);
 }
 
-static inline vector2f32 vector2f32_create(f32 x, f32 y) {
-	return (vector2f32){x, y};
+static inline vector2f32 vector2f32_make(f32 x, f32 y) {
+	return (vector2f32){.x = x, .y = y};
 }
 
-static inline f32 vector2f32_magnitude(vector2f32 x) {
+static inline f32 vector2f32_mag(vector2f32 x) {
 	return sqrtf32(x.x * x.x + x.y * x.y);
 }
 
-static inline vector2f32 vector2f32_scalar_multiply(vector2f32 v, f32 s) {
+static inline vector2f32 vector2f32_scalar_mul(vector2f32 v, f32 s) {
 	return (vector2f32){.x = s * v.x, .y = s * v.y};
 }
 
@@ -38,11 +46,11 @@ static inline vector2f32 vector2f32_scalar_add(vector2f32 v, f32 s) {
 	return (vector2f32){.x = v.x + s, .y = v.y + s};
 }
 
-static inline vector2f32 vector2f32_scalar_subtract(vector2f32 v, f32 s) {
+static inline vector2f32 vector2f32_scalar_sub(vector2f32 v, f32 s) {
 	return (vector2f32){.x = v.x - s, .y = v.y - s};
 }
 
-static inline vector2f32 vector2f32_scalar_divide(vector2f32 v, f32 s) {
+static inline vector2f32 vector2f32_scalar_div(vector2f32 v, f32 s) {
 	assert(s != 0.0f);
 	return (vector2f32){.x = v.x / s, .y = v.y / s};
 }
