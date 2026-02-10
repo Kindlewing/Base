12e1fd29fd34482aff44f7c58a32e0583fe0f881
Hudson Finn <hudson.finn@zohomail.com>
2026-02-10 12:25:30 -0500

    Linear algebra stuff

--- include/base_math.h
+++ include/base_math.h
@@ -11,57 +11,10 @@
 #define BASE_DEG2RAD 0.01745329251994329577f // PI/180
 #define BASE_RAD2DEG 57.2957795130823208768f // 180/PI
 
-typedef struct vector2f32 {
-	f32 x;
-	f32 y;
-} vector2f32;
-
-typedef struct vector3f32 {
-	f32 x;
-	f32 y;
-	f32 z;
-} vector3f32;
-
-typedef struct vector4f32 {
-	f32 r;
-	f32 g;
-	f32 b;
-	f32 a;
-} vector4f32;
-
 static inline f32 base_sqrtf32(f32 x) {
 	__m128 t = _mm_set_ss(x);
 	t = _mm_sqrt_ss(t);
 	return _mm_cvtss_f32(t);
-}
-
-typedef struct mat3x3f32 {
-	f32 m[9]; // idx: col * 3 + row
-} mat3x3f32;
-
-typedef struct mat4x4f32 {
-	f32 m[16]; // idx: col * 4 + row
-} mat4x4f32;
-
-static inline f32 vector2f32_mag(vector2f32 x) {
-	return base_sqrtf32(x.x * x.x + x.y * x.y);
-}
-
-static inline vector2f32 vector2f32_scalar_mul(vector2f32 v, f32 s) {
-	return (vector2f32){.x = s * v.x, .y = s * v.y};
-}
-
-static inline vector2f32 vector2f32_scalar_add(vector2f32 v, f32 s) {
-	return (vector2f32){.x = v.x + s, .y = v.y + s};
-}
-
-static inline vector2f32 vector2f32_scalar_sub(vector2f32 v, f32 s) {
-	return (vector2f32){.x = v.x - s, .y = v.y - s};
-}
-
-static inline vector2f32 vector2f32_scalar_div(vector2f32 v, f32 s) {
-	assert(s != 0.0f);
-	return (vector2f32){.x = v.x / s, .y = v.y / s};
 }
 
 #endif
---
+++ include/linalg.h
@@ -0,0 +1,69 @@
+#ifndef LINALG_H
+#define LINALG_H
+
+#include "typedefs.h"
+#include "base_math.h"
+
+typedef struct vector2f32 {
+	f32 x;
+	f32 y;
+} vector2f32;
+
+typedef struct vector3f32 {
+	f32 x;
+	f32 y;
+	f32 z;
+} vector3f32;
+
+typedef struct vector4f32 {
+	f32 r;
+	f32 g;
+	f32 b;
+	f32 a;
+} vector4f32;
+
+typedef struct mat3x3f32 {
+	f32 m[9]; // idx: col * 3 * row
+} mat3x3f32;
+
+typedef struct mat4x4f32 {
+	f32 m[16]; // idx: col * 4 * row
+} mat4x4f32;
+
+/* CREATION */
+
+vector2f32 vector2f32_make(f32 x, f32 y) {
+	return (vector2f32){.x = x, .y = y};
+}
+
+static inline vector3f32 vector3f32_make(f32 x, f32 y, f32 z) {
+	return (vector3f32){.x = x, .y = y, .z = z};
+}
+
+vector4f32 vector4f32_make(f32 r, f32 g, f32 b, f32 a) {
+	return (vector4f32){.r = r, .g = g, .b = b, .a = a};
+}
+
+/* OPERATIONS */
+static inline f32 vector2f32_mag(vector2f32 x) {
+	return base_sqrtf32(x.x * x.x + x.y * x.y);
+}
+
+static inline vector2f32 vector2f32_scalar_mul(vector2f32 v, f32 s) {
+	return (vector2f32){.x = s * v.x, .y = s * v.y};
+}
+
+static inline vector2f32 vector2f32_scalar_add(vector2f32 v, f32 s) {
+	return (vector2f32){.x = v.x + s, .y = v.y + s};
+}
+
+static inline vector2f32 vector2f32_scalar_sub(vector2f32 v, f32 s) {
+	return (vector2f32){.x = v.x - s, .y = v.y - s};
+}
+
+static inline vector2f32 vector2f32_scalar_div(vector2f32 v, f32 s) {
+	assert(s != 0.0f);
+	return (vector2f32){.x = v.x / s, .y = v.y / s};
+}
+
+#endif
