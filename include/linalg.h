0c08dff427defb8b32cbcd328e6673ef8df5cf40
Hudson Finn <hudson.finn@zohomail.com>
2026-02-10 20:53:58 -0500

    Linear algrebra stuff

--- include/linalg.h
+++ include/linalg.h
@@ -4,6 +4,11 @@
 #include "typedefs.h"
 #include "base_math.h"
 
+// matrix3 index
+#define MAT3_IDX(m, r, c) ((m).m[(c) * 3 + (r)])
+// matrix4 index
+#define MAT4_IDX(m, r, c) ((m).m[(c) * 4 + (r)])
+
 typedef struct vector2f32 {
 	f32 x;
 	f32 y;
@@ -23,11 +28,11 @@ typedef struct vector4f32 {
 } vector4f32;
 
 typedef struct mat3x3f32 {
-	f32 m[9]; // idx: col * 3 * row
+	f32 m[9]; // idx: col * 3 + row
 } mat3x3f32;
 
 typedef struct mat4x4f32 {
-	f32 m[16]; // idx: col * 4 * row
+	f32 m[16]; // idx: col * 4 + row
 } mat4x4f32;
 
 /* CREATION */
@@ -42,6 +47,27 @@ static inline vector3f32 vector3f32_make(f32 x, f32 y, f32 z) {
 
 static inline vector4f32 vector4f32_make(f32 r, f32 g, f32 b, f32 a) {
 	return (vector4f32){.r = r, .g = g, .b = b, .a = a};
+}
+
+static inline mat3x3f32 mat3f32_identity() {
+	// clang-format off
+	return (mat3x3f32){.m = {
+		1, 0, 0,
+		0, 1, 0,
+		0, 0, 1
+	}};
+	// clang-format on
+}
+
+static inline mat4x4f32 mat4f32_identity() {
+	// clang-format off
+	return (mat4x4f32){.m = {
+		1, 0, 0, 0,
+		0, 1, 0, 0,
+		0, 0, 1, 0,
+		0, 0, 0, 1
+	}};
+	// clang-format on
 }
 
 /* OPERATIONS */
