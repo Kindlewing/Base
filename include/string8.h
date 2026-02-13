352b0542f113edff8f58a6fd5d5eedea54c32cef
Hudson Finn <hudson.finn@zohomail.com>
2026-02-13 14:36:12 -0500

    String8 to cstring

--- include/string8.h
+++ include/string8.h
@@ -16,4 +16,13 @@ string8 string8_make(arena *a, const char *cstring);
 string8 string8_read_file(arena *a, int fd, i64 size);
 b8 string8_eq(string8 *s1, string8 *s2);
 
+static inline char *string8_to_cstr(arena *a, string8 s) {
+	char *cstr = arena_push_array(a, char, s.length + 1);
+	for(size_t i = 0; i < s.length; i++) {
+		cstr[i] = s.data[i];
+	}
+	cstr[s.length] = '\0';
+	return cstr;
+}
+
 #endif // STRING8_H
